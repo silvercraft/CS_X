f875be2ca086b1d48a4b5ff9f1e9c9058a4d47bd
RNGSilvercraft <yi88613@gmail.com>
2018-09-18 11:24:38 +0800

    what

--- week_1/hw_2.R
+++ week_1/hw_2.R
@@ -8,11 +8,24 @@
 que=sample(0:9,4)
 print(que)
 counter=0
+ans=c(-1:-1)
 a = 0
 while(a != 4){
-  for(i in 1:4){
+  i = 1
+  while(i < 5){
     ans[i]=readline("enter DIFFERENT numbers: ")
+    if(i>1){
+      for(j in 1:(i-1)){
+        if(as.integer(ans[j])==as.integer(ans[i])){
+          print("reenter the number")
+          i=i-1
+          break
+        }
+      }
+    }
+    i<-i+1
   }
+  
   a=0
   b=0
   for(i in 1:4){
