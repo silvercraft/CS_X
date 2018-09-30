21e9f767b0ec158e762e362d39d06a42ccb2794e
RNGSilvercraft <yi88613@gmail.com>
2018-10-01 02:19:17 +0800

    yup

--- week_3/hw_3/hw_3.R
+++ week_3/hw_3/hw_3.R
@@ -1,16 +1,30 @@
-mydata = read.csv(file="C:/Users/andy/Documents/CS+X/CS_X/week_3/hw_3/vocabulary.csv")  # read csv file 
-library(ggplot2)
-theme_set(theme_classic())
-gg = ggplot(mydata, aes(x=as.numeric(Index),y=as.numeric(TrainVideoCount),color=Name))+
-  geom_point() + 
-  #geom_smooth()
-  xlim(c(0, 4000)) + 
-  ylim(c(0, 2500)) + 
-  labs(subtitle="Index to Vids", 
-       y="TrainVideoCount", 
-       x="Index", 
-       title="text", 
-       caption = "Source: google")
 
+mydata = head(read.csv(file="vocabulary.csv"),180)  # read csv file 
+library(ggplot2)
+
+theme_set(theme_classic())
+gg = ggplot(data=mydata, aes(x=Vertical1))+
+  geom_bar(aes(fill=Vertical1,colour = "white"))+
+  coord_flip()+
+  labs(x="catagories", 
+       title="Videos catagory counts")
 plot(gg)
 
+qplot(TrainVideoCount,Vertical1,data=mydata)
+
+gg = ggplot(data=mydata, aes(x=as.numeric(Index),y=TrainVideoCount),color=Vertical1)+
+  geom_point()+
+  geom_smooth(color = "red",size = 2)+
+  labs(x="index(1 to 180)", y="videocount",
+       title="Videos catagory counts")
+plot(gg)
+
+gg = ggplot(data=mydata, aes(x=as.numeric(Index),y=log(TrainVideoCount)))+
+  geom_point(aes(col=Vertical1))+
+  geom_smooth(color = "yellow",size = 1)+
+  labs(x="index(1 to 180)",y="videocount in log" ,
+       title="Videos catagory counts")
+plot(gg)
+
+mydata[1:180,c(1,2,6), drop=FALSE]
+print("source:https://research.google.com/youtube8m/")
